9be0b7d72f08039fc3dd2bd0a9a72e8f93417a24
sathish <sathishavm@gmail.com>
2018-07-03 13:38:05 -0700

    PR recommends and fixes

--- lib/samson/process_utils.rb
+++ lib/samson/process_utils.rb
@@ -2,30 +2,21 @@
 
 module Samson
   module ProcessUtils
-    ATTRIBUTES = ['pid', 'ppid', 'gid', 'user', 'start', 'args'].freeze
-
-    class Process
-      attr_accessor *ATTRIBUTES
-
-      def initialize(args)
-        ATTRIBUTES.each_with_index do |name, index|
-          instance_variable_set("@#{name}", args[index])
-        end
-      end
-    end
+    ATTRIBUTES = ['pid', 'ppid', 'gid', 'pcpu', 'user', 'start', 'args'].freeze
+    WHITELISTS = ['puma'].freeze
 
     class << self
       def ps_list
-        pipe = IO.popen("ps -eo #{ATTRIBUTES.join(',')}")
+        pipe = IO.popen("ps -eo #{ATTRIBUTES.join(',')} | grep -v #{WHITELISTS.join(',')}")
         pipe.readlines[1..-1].map do |line|
-          Process.new line.lstrip.split(/\s+/, 6)
+          Hash[ATTRIBUTES.zip line.lstrip.split(/\s+/, 7)]
         end
       end
 
       def report_to_statsd
         ps_list.each do |process|
-          timeout = Time.parse(process.start) < Time.now - Rails.application.config.samson.deploy_timeout
-          tags = ATTRIBUTES.map { |attr| "#{attr}:#{process.send(attr)}" }
+          timeout = Time.parse(process.fetch('start')) < Time.now - Rails.application.config.samson.deploy_timeout
+          tags = ATTRIBUTES.map { |attr| "#{attr}:#{process.fetch(attr)}" }
           Samson.statsd.gauge("process.start.time", (timeout ? 2.0 : 1.0), tags: tags)
         end
       end
--- test/lib/samson/process_utils_test.rb
+++ test/lib/samson/process_utils_test.rb
@@ -8,12 +8,12 @@ describe Samson::ProcessUtils do
   describe '.ps_list' do
     it 'returns a list of running processes' do
       IO.popen("echo samson_testing; sleep 5")
-      Samson::ProcessUtils.ps_list.map(&:args).must_include "sh -c echo samson_testing; sleep 5\n"
+      Samson::ProcessUtils.ps_list.map { |ps| ps['args'] }.must_include "sh -c echo samson_testing; sleep 5\n"
     end
 
     it 'skips the completed processes' do
       IO.popen("echo samson_completed")
-      Samson::ProcessUtils.ps_list.map(&:args).wont_include "sh -c echo samson_completed\n"
+      Samson::ProcessUtils.ps_list.map { |ps| ps['args'] }.wont_include "sh -c echo samson_completed\n"
     end
   end
 
@@ -21,8 +21,8 @@ describe Samson::ProcessUtils do
     it 'report the processes to statsd' do
       IO.popen("echo samson_statsd; sleep 5")
       Samson::ProcessUtils.ps_list.each do |process|
-        tags = ["pid:#{process.pid}", "ppid:#{process.ppid}", "gid:#{process.gid}",
-                "user:#{process.user}", "start:#{process.start}", "args:#{process.args}"]
+        tags = ["pid:#{process['pid']}", "ppid:#{process['ppid']}", "gid:#{process['gid']}", "pcpu:#{process['pcpu']}",
+                "user:#{process['user']}", "start:#{process['start']}", "args:#{process['args']}"]
         Samson.statsd.stubs(:gauge).with('process.start.time', anything, tags: tags)
       end
       Samson.statsd.stubs(:gauge).with('process.start.time', anything, anything)
